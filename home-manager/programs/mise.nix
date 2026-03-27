c92f8bc6487282b2e7372ee911a71f00840d683c
Luke Hansford <mail@lukehansford.me>
2026-03-27 13:43:09 +0800

    Handle mise config

--- home-manager/programs/mise.nix
+++ home-manager/programs/mise.nix
@@ -8,5 +8,20 @@
   programs.mise = {
     enable = true;
     enableZshIntegration = true;
+
+    globalConfig = {
+      tools = {
+        node = "24";
+      };
+    };
+
+    settings = {
+      idiomatic_version_file_enable_tools = [
+        "python"
+        "ruby"
+        "terraform"
+        "node"
+      ];
+    };
   };
 }

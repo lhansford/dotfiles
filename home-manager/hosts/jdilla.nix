8d3d87fe4c5e2a712c444dd8f582cbf4c9d6987c
Luke Hansford <mail@lukehansford.me>
2026-03-27 08:39:40 +0800

    Fix nvidia set up

--- home-manager/hosts/jdilla.nix
+++ home-manager/hosts/jdilla.nix
@@ -4,9 +4,13 @@
   imports = [
     ../environments/graphical.nix
   ];
-  # Keep in sync with CachyOS updates. See the home-manager docs.
+
+  # Nvidia support
+  nixpkgs.config.nvidia.acceptLicense = true;
   targets.genericLinux.gpu.nvidia = {
     enable = true;
+
+    # Keep in sync with CachyOS updates. See the home-manager docs.
     version = "595.45.04";
     sha256 = "sha256-zUllSSRsuio7dSkcbBTuxF+dN12d6jEPE0WgGvVOj14=";
   };

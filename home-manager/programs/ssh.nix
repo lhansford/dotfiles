c734280bc81561a23643741082a0016809dda1c0
Luke Hansford <mail@lukehansford.me>
2026-03-30 08:53:50 +0800

    SSH fixes

--- home-manager/programs/ssh.nix
+++ home-manager/programs/ssh.nix
@@ -4,10 +4,33 @@ _:
   programs.ssh = {
     enable = true;
     enableDefaultConfig = false;
-    matchBlocks."*" = {
-      extraOptions = {
-        AddKeysToAgent = "yes";
-        IdentitiesOnly = "yes";
+
+    matchBlocks = {
+      "github.com" = {
+        hostname = "github.com";
+        user = "git";
+        identityAgent = "~/.1password/agent.sock";
+        extraOptions = {
+          # We disable IdentitiesOnly for GitHub so it can "see" the keys inside the 1Password agent.
+          IdentitiesOnly = "no";
+        };
+      };
+
+      "codeberg.org" = {
+        hostname = "codeberg.org";
+        user = "git";
+        identityAgent = "~/.1password/agent.sock";
+        extraOptions = {
+          # We disable IdentitiesOnly for GitHub so it can "see" the keys inside the 1Password agent.
+          IdentitiesOnly = "no";
+        };
+      };
+
+      "*" = {
+        extraOptions = {
+          IdentityAgent = "~/.1password/agent.sock";
+          IdentitiesOnly = "yes";
+        };
       };
     };
   };

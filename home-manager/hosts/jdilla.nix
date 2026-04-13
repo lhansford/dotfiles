3b1f90b7e8ef100628fe04d4730838982513368b
Luke Hansford <mail@lukehansford.me>
2026-04-13 11:03:59 +0800

    Fix GPU issues

--- home-manager/hosts/jdilla.nix
+++ home-manager/hosts/jdilla.nix
@@ -11,7 +11,10 @@
     enable = true;
 
     # Keep in sync with CachyOS updates. See the home-manager docs.
-    version = "595.45.04";
-    sha256 = "sha256-zUllSSRsuio7dSkcbBTuxF+dN12d6jEPE0WgGvVOj14=";
+    # Run the following to check the version:
+    # nvidia-smi --query-gpu=driver_version --format=csv,noheader
+    # Then provide a fake SHA ("sha256-AAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAA=") to get Nix to give us the real one.
+    version = "595.58.03";
+    sha256 = "sha256-jA1Plnt5MsSrVxQnKu6BAzkrCnAskq+lVRdtNiBYKfk=";
   };
 }

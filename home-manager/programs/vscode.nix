7a122d9c99a95f2eebb373ae7087935da36feeaa
Luke Hansford <mail@lukehansford.me>
2026-04-19 21:39:13 +0800

    Add vscode settings

--- home-manager/programs/vscode.nix
+++ home-manager/programs/vscode.nix
@@ -8,15 +8,29 @@
   programs.vscode = {
     enable = true;
     package = pkgs.vscodium;
-    profiles.default.extensions = with pkgs.nix-vscode-extensions.open-vsx; [
-      hashicorp.terraform
-      jnoortheen.nix-ide
-      esbenp.prettier-vscode
-      hverlin.mise-vscode
-      golang.go
-      rust-lang.rust-analyzer
-      tamasfe.even-better-toml
-      wayou.vscode-todo-highlight
-    ];
+    profiles = {
+      default = {
+        extensions = with pkgs.nix-vscode-extensions.open-vsx; [
+              dbaeumer.vscode-eslint
+              esbenp.prettier-vscode
+              golang.go
+              hashicorp.terraform
+              hverlin.mise-vscode
+              jnoortheen.nix-ide
+              rust-lang.rust-analyzer
+              svelte.svelte-vscode
+              tamasfe.even-better-toml
+              tauri-apps.tauri-vscode
+              wayou.vscode-todo-highlight
+            ];
+        userSettings = {
+          "[rust]"."editor.tabSize" = 2;
+          "[svelte]"."editor.tabSize" = 2;
+          "[svelte]"."editor.defaultFormatter" = "esbenp.prettier-vscode";
+          "[typescript]"."editor.tabSize" = 2;
+          "[typescript]"."editor.defaultFormatter" = "esbenp.prettier-vscode";
+        };
+      };
+    };
   };
 }
